7951079f23a24c23209f0c680afa945280dc1800
harrison <d.harrison.lima@gmail.com>
2024-09-22 22:44:33 -0300

    feat: add resume and resumeItem

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header";
+import Resume from "./components/Resume";
+
+const income = 10;
+const expense = 50;
+const total = 70;
 
 function App() {
   return (
     <>
       <Header />
+      <Resume income={income} expense={expense} total={total} />
     </>
   );
 }
